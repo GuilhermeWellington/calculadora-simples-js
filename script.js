66cfad5258b3ce6079c6ae1e04f2576ded60665d
Guilherme <guilhermewellington959@gmail.com>
2026-04-30 14:00:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function calcular(numero1, numero2, operacao, saida) {
+    var num1 = document.getElementById(numero1).value;
+    var num2 = document.getElementById(numero2).value;
+    var operador = document.getElementById(operacao).options[document.getElementById(operacao).selectedIndex].value;
+    var expressao = num1 + operador + num2;
+    resultado = eval(expressao);
+    document.getElementById(saida).innerHTML = resultado;
+}
+
+function limpar(id1, id2) {
+    document.getElementById(id1).value = "";
+    document.getElementById(id2).value = "";
+}
